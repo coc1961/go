3eddf1b77c2536e9fe61078f8421396cb40559e4
Carlos Castro <carlos_o_castro@yahoo.com.ar>
2018-09-14 21:51:59 -0300

    Add trs and adds Tools

--- adds/main.go
+++ adds/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	if len(os.Args) < 4 {
 		fmt.Fprintf(os.Stderr, "\x1b[93;1mAgrega un texto antes o despues de una determinada linea\n")
-		fmt.Fprintf(os.Stderr, "el primer parametro es -a , -b , -r (After/Before/Replace Line)\n")
+		fmt.Fprintf(os.Stderr, "el primer parametro es -a , -b , -r, -x (After/Before/Replace Line/Replace RexExp)\n")
 		fmt.Fprintf(os.Stderr, "el segundo parametro es la expresion regular a buscar\n")
 		fmt.Fprintf(os.Stderr, "el tercero y sucesivos parametro es el texto a agregar\n")
 		fmt.Fprintf(os.Stderr, "\n\x1b[37;1madds [-a,-b] \"regexporigen\" \"textoagregado\" ... \"textoagregado_n\"\x1b[0m\n\n")
@@ -27,8 +27,8 @@ func main() {
 		return
 	}
 
-	if !(pos == "-a" || pos == "-b" || pos == "-r") {
-		fmt.Fprintf(os.Stderr, "Opcion Invalida, opciones posibles -a -b -r\n")
+	if !(pos == "-a" || pos == "-b" || pos == "-r" || pos == "-x") {
+		fmt.Fprintf(os.Stderr, "Opcion Invalida, opciones posibles -a -b -r -x\n")
 		return
 	}
 
@@ -37,28 +37,31 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	line := ""
 	for scanner.Scan() {
-		printLine(line, pos, in, ou)
+		print(printLine(line, pos, in, ou))
 		line = scanner.Text()
 	}
-	printLine(line, pos, in, ou)
+	print(printLine(line, pos, in, ou))
 
 }
 
-func printLine(line string, pos string, in *regexp.Regexp, ou string) {
+func print(s1, s2 string) {
+	fmt.Println(s1)
+	if s2 != "" {
+		fmt.Println(s2)
+	}
+}
+func printLine(line string, pos string, in *regexp.Regexp, ou string) (string, string) {
 	ok := in.MatchString(line)
 	switch {
 	case ok && pos == "-a":
-		fmt.Println(line)
-		fmt.Println(ou)
-		return
+		return line, ou
 	case ok && pos == "-b":
-		fmt.Println(ou)
-		fmt.Println(line)
-		return
+		return ou, line
 	case ok && pos == "-r":
-		fmt.Println(ou)
-		return
+		return ou, ""
+	case ok && pos == "-x":
+		return in.ReplaceAllString(line, ou), ""
 	default:
-		fmt.Println(line)
+		return line, ""
 	}
 }
---
+++ adds/main_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"regexp"
+	"testing"
+)
+
+func Test_printLine(t *testing.T) {
+	type args struct {
+		line string
+		pos  string
+		in   *regexp.Regexp
+		ou   string
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  string
+		want1 string
+	}{
+		{
+			// Prueba con dos grupos y un texto
+			// (.*?Server=) Grupo 1
+			// (.*?) Grupo2
+			// \bpapa\b Palabra Prueba
+			// Imprimo Grupo1 nueva_palabra grupo2
+			// ${1}Prueba${2}
+			"Test Replace Regexp",
+			args{"Server=papa papa", "-x", regexp.MustCompile(`^(.*?Server=)\bpapa\b(.*?)$`), `${1}Prueba${2}`},
+			"Server=Prueba papa",
+			"",
+		},
+		{
+			"Test Replace Line",
+			args{"Server=papa papa", "-r", regexp.MustCompile(`Server`), `Prueba`},
+			"Prueba",
+			"",
+		},
+		{
+			"Test Replace Line",
+			args{"Server=papa papa", "-b", regexp.MustCompile(`Server`), `Prueba`},
+			"Prueba",
+			"Server=papa papa",
+		},
+		{
+			"Test Replace Line",
+			args{"Server=papa papa", "-a", regexp.MustCompile(`Server`), `Prueba`},
+			"Server=papa papa",
+			"Prueba",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := printLine(tt.args.line, tt.args.pos, tt.args.in, tt.args.ou)
+			if got != tt.want {
+				t.Errorf("printLine() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("printLine() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
