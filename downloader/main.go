31fc4fc275c19915bcdbbf8206087af91293f061
Carlos Castro <carlos_o_castro@yahoo.com.ar>
2018-09-25 20:06:07 -0300

    Add trs and adds Tools

--- downloader/main.go
+++ downloader/main.go
@@ -12,7 +12,7 @@ import (
 	"time"
 )
 
-var progressBarrArray []*progressReader
+var progressBarArray []*progressReader
 
 // BUG(carlos): Manage Errors!
 func main() {
@@ -56,16 +56,20 @@ func main() {
 		log.Fatalf("remote server content-length is invalid")
 	}
 
-	progressBarrArray = make([]*progressReader, 0)
+	// Inicializo Variables
+	progressBarArray = make([]*progressReader, 0)
 	partialDownloadArray := make([]*partialDownload, 0)
 
+	// Reservo Espacio en el Archivo de Salida
 	out.Truncate(contentLength)
 
+	// Calculo el tamaño del chunk tamaño / hilos
 	chunkSize := contentLength / workers
 
 	wg := sync.WaitGroup{}
 	wg.Add(int(workers))
 
+	// Creo los downloaders parciales
 	for i := int64(0); i < workers; i++ {
 		chunkStart, chunkEnd := chunkSize*i, (chunkSize*i)+chunkSize-1
 		if i+1 == workers {
@@ -74,13 +78,16 @@ func main() {
 
 		tmp := createPartialDownload(resourceURL, chunkStart, chunkEnd, out)
 		partialDownloadArray = append(partialDownloadArray, tmp)
+
+		// Comienzo Descarga
 		go tmp.Download(&wg)
 	}
 
+	// Muestro Barra de Progreso
 	go func() {
 		for {
 			fmt.Print("\rProgress [ ")
-			for _, v := range progressBarrArray {
+			for _, v := range progressBarArray {
 				fmt.Print(v.pos / (v.len / 100))
 				fmt.Print("% ")
 			}
@@ -91,13 +98,16 @@ func main() {
 
 	wg.Wait()
 
+	// Cierro archivo de salida
 	out.Close()
 
+	// Cierro archivos parciales
 	for _, v := range partialDownloadArray {
 		if v.out != nil {
 			v.out.Close()
 		}
 	}
+	// Hay Errores ?
 	for _, v := range partialDownloadArray {
 		if v.err != nil {
 			go os.Remove(out.Name())
@@ -105,31 +115,37 @@ func main() {
 		}
 	}
 	time.Sleep(time.Millisecond * 10)
+	//End!
 	fmt.Println("\nEnd...")
 
 }
 
+// Estructura de descarga parcial
 type partialDownload struct {
-	resourceURL *url.URL
-	rangeHeader string
-	out         *os.File
-	err         error
-	len         int64
-	pos         int64
+	resourceURL *url.URL // Url de descarga
+	rangeHeader string   // bytes a descargar
+	out         *os.File // archivo de salida
+	err         error    // error ?
+	len         int64    // bytes a descargar
+	pos         int64    // posicion de inicio
 }
 
+// Creo partialDownload
 func createPartialDownload(resourceURL *url.URL, chunkStart int64, chunkEnd int64, out *os.File) *partialDownload {
 	rangeHeader := fmt.Sprintf("bytes=%d-%d", chunkStart, chunkEnd)
 	return &partialDownload{resourceURL, rangeHeader, out, nil, chunkEnd - chunkStart, chunkStart}
 }
 
+// Descarga Parcial
 func (p *partialDownload) Download(wg *sync.WaitGroup) {
 	defer wg.Done()
+	// Request
 	req, error := http.NewRequest("GET", p.resourceURL.String(), nil)
 	if error != nil {
 		p.err = error
 		return
 	}
+	// Seteo rango de descarga
 	req.Header.Add("Range", p.rangeHeader)
 	var client http.Client
 	resp, error := client.Do(req)
@@ -138,6 +154,7 @@ func (p *partialDownload) Download(wg *sync.WaitGroup) {
 		return
 	}
 
+	// Archivo de descarga parcial
 	out, err := os.OpenFile(p.out.Name(), os.O_RDWR, os.ModePerm)
 	if err != nil {
 		p.err = err
@@ -145,27 +162,36 @@ func (p *partialDownload) Download(wg *sync.WaitGroup) {
 	}
 	p.out = out
 
+	// Me posiciono en la posicion en donde debo descargar
 	p.out.Seek(p.pos, os.SEEK_SET)
 
+	// Creo un emboltorio de reader para que setee valores
+	// de progreso de descarga para alimentar la barra de %
 	wrapReader := createProgressReader(&resp.Body, p.len)
 
-	progressBarrArray = append(progressBarrArray, wrapReader)
+	// Agrego al array para desplegar el porc de descarga
+	progressBarArray = append(progressBarArray, wrapReader)
+
+	// Descargo!
 	_, error = io.Copy(p.out, wrapReader)
 	if error != nil {
 		p.err = error
 	}
 }
 
+// Creo el objeto para alimentar la barra de progreso
 func createProgressReader(reader *io.ReadCloser, len int64) *progressReader {
 	return &progressReader{reader, len, 0}
 }
 
+// Enboltorio de reader que guarda el % de descarga
 type progressReader struct {
-	reader *io.ReadCloser
-	len    int64
-	pos    int64
+	reader *io.ReadCloser // Reader original
+	len    int64          // total a descargar
+	pos    int64          // bytes procesados
 }
 
+// Actualizo % de descarga y realizo la lectura real
 func (r *progressReader) Read(p []byte) (n int, err error) {
 	rr := *(r.reader)
 	lei, err := rr.Read(p)
