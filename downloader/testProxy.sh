380179b2f4cc4e11c397e83acbd3d2df240a941c
Carlos Castro <carlos_o_castro@yahoo.com.ar>
2018-11-21 20:09:54 -0300

    Add trs and adds Tools

--- downloader/testProxy.sh
+++ downloader/testProxy.sh
@@ -5,7 +5,7 @@ docker run -d --rm --name mycntlm -e CNTLM_PROXY="proxy:8100" -e CNTLM_NO_PROXY=
 export http_proxy=http://127.0.0.1:8888
 
 export GOMAXPROCS=8
-go run  main.go -n 5 -v -url http://mirror.cc.columbia.edu/pub/software/eclipse/technology/epp/downloads/release/luna/SR2/eclipse-standard-luna-SR2-win32-x86_64.zip  -o eclipse.zip && unzip -t eclipse.zip > /dev/null && echo Test OK
+go run  main.go -n 5 -v -url http://eclipse.c3sl.ufpr.br/technology/epp/downloads/release/2018-09/R/eclipse-jee-2018-09-win32-x86_64.zip  -o eclipse.zip && unzip -t eclipse.zip > /dev/null && echo Test OK
 rm eclipse.zip 2> /dev/null
 
 
