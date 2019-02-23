881f24058944d67ffd7dd2f2cfadc5b28992de62
Carlos Castro <carlos_o_castro@yahoo.com.ar>
2019-02-23 13:16:57 -0300

    Downloader a su propio proyecto

--- downloader/test.sh
+++
@@ -1,3 +0,0 @@
-export GOMAXPROCS=8
-go run  main.go -n 5 -v -url http://eclipse.c3sl.ufpr.br/technology/epp/downloads/release/2018-09/R/eclipse-jee-2018-09-win32-x86_64.zip  -o eclipse.zip && unzip -t eclipse.zip > /dev/null && echo Test OK
-rm eclipse.zip 2> /dev/null
